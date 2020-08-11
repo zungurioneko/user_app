d9349ddf66c4712aaaa30be97486885d6db88ebb
zungurioneko <zungurioneko@gmail.com>
2020-08-11 21:59:24 +0900

    Post モデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 256 }
+  validates :age, presence: true, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 150 }
 end
