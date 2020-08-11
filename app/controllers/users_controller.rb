587ba911682eed5b78ef13efd651f7d4aff65426
zungurioneko <zungurioneko@gmail.com>
2020-08-11 20:55:50 +0900

    編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    redirect_to user
   end
 
   def destroy
