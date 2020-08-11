51fcd6fac517de0b8d73fb88348042046f4e0241
zungurioneko <zungurioneko@gmail.com>
2020-08-11 20:15:02 +0900

    詳細表示機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.create!(post_params)
+    user = User.create!(user_params)
     redirect_to user
   end
 
