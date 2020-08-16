83294f6c24b2e2ab9577f963e028da22cfa4eaf2
zungurioneko <zungurioneko@gmail.com>
2020-08-16 10:03:14 +0900

    Bootstrap と FontAwesome をインポート

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap/dist/js/bootstrap")
+require("@fortawesome/fontawesome-free/js/all")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
